826e7120cdd84c30db02b85c13493353d5f6cdb2
frallanosr <frallanosr@gmail.com>
2017-04-20 18:11:20 -0300

    Agrege la carpeta servicio en la capa logica con su respectiva clase y modifique la pagina de agrega servicio

--- ControlValesAlimentacion/CapaVisualizacion/agregaServicio.aspx.designer.cs
+++ ControlValesAlimentacion/CapaVisualizacion/agregaServicio.aspx.designer.cs
@@ -56,5 +56,14 @@ namespace CapaVisualizacion {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox valorDelServicio;
+        
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
